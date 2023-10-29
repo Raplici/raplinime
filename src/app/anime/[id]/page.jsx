b2ed7e29f89331d9b9376bdbfc8205a41335cb36
Rafli Ferdian <raflitsany35@gmail.com>
2023-10-29 21:06:34 +0700

    SESI6 - Nge-Trick Nested Data

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -1,4 +1,4 @@
-import { getAnimeResponse } from "@/app/libs/api-libs";
+import { getAnimeResponse } from "@/libs/api-libs";
 import VideoPlayer from "@/components/Utilities/VideoPalyer";
 import Image from "next/image";
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,17 @@
 import AnimeList from "@/components/AnimeList";
 import Header from "@/components/AnimeList/Header";
-import { getAnimeResponse } from "@/app/libs/api-libs";
+import { getAnimeResponse, getNestedAnimeResponse } from "@/libs/api-libs";
 
 const Page = async () => {
   const topAnime = await getAnimeResponse("top/anime", "limit=8");
+  let recommendedAnime = await getNestedAnimeResponse(
+    "recommendations/anime",
+    "entry"
+  );
+  recommendedAnime = { data: recommendedAnime.slice(0, 4) };
 
   return (
     <>
-      {/* populer */}
       <section>
         <Header
           title="Paling Populer"
@@ -15,6 +19,10 @@ const Page = async () => {
           linkTitle="Lihat Semua"
         />
         <AnimeList api={topAnime} />
+      </section>
+      <section>
+        <Header title="Rekomendasi" />
+        <AnimeList api={recommendedAnime} />
       </section>
     </>
   );
