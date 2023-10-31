85ac4e877ca1cbcf0d8f7b065ee72022700b2940
Rafli Ferdian <raflitsany35@gmail.com>
2023-10-31 12:34:24 +0700

    update group component

--- src/components/Group/index.jsx
+++ src/components/Group/index.jsx
@@ -1,18 +1,16 @@
-const Group = ({ title, children }) => {
+const Group = ({ title, children, className, ...rest }) => {
+  const containerClassName = title
+    ? "w-full relative rounded-2xl border border-Black-15 px-4 pb-4 pt-8 flex flex-col gap-4 md:p-12 md:gap-5"
+    : "w-full relative rounded-2xl border border-Black-15 px-4 pb-4 pt-4 flex flex-col gap-4 md:p-12 md:gap-5";
+
   return (
-    <div>
+    <div className={`${containerClassName} ${className}`} {...rest}>
       {title ? (
-        <div className="w-full relative rounded-2xl mt-4 border border-Black-15 px-4 md:px-8 pb-4 md:py-9 pt-9 flex flex-col gap-4 md:gap-5">
-          <label className="text-base leading-6 absolute -top-3.5 left-4 rounded-lg bg-Red-45 px-4 py-2 md:px-6 md:py-2.5 font-bold text-Absolute-White md:text-xl md:leading-7 md:-top-5 md:left-8">
-            {title}
-          </label>
-          {children}
+        <div className="text-base leading-6 absolute -top-3.5 left-4 rounded-lg bg-Red-45 px-4 py-2 md:px-6 md:py-2.5 font-bold text-Absolute-White md:text-xl md:leading-7 md:-top-5 md:left-8">
+          {title}
         </div>
-      ) : (
-        <div className="w-full relative rounded-2xl mt-4 border border-Black-15 px-4 md:p-8 pb-4 pt-4 flex flex-col gap-4 md:gap-5">
-          {children}
-        </div>
-      )}
+      ) : null}
+      {children}
     </div>
   );
 };
