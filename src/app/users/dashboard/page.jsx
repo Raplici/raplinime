8188bf455c1b60526f034923872ef532d263d0a2
Rafli Ferdian <raflitsany35@gmail.com>
2023-11-30 16:15:42 +0700

    SESI9 - Validasi User Dashboard

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,14 +1,31 @@
 import { authUserSession } from "@/libs/auth-libs";
 import Image from "next/image";
+import Link from "next/link";
 
 const Page = async () => {
   const user = await authUserSession();
-  console.log(user);
+
   return (
-    <div className="text-color-primary">
-        <h3>DASHBOARD</h3>
-        <h3>Hi, {user.name}</h3>
-      <Image src={user.image} alt="" width={250} height={250}/>
+    <div className="text-color-primary flex flex-col justify-center items-center mt-8">
+      <h3 className="text-2xl font-bold">Welcome, {user?.name}</h3>
+
+      <Image src={user?.image} alt="" width={250} height={250} />
+
+      <div className="flex flex-wrap gap-4 py-8">
+        <Link
+          href="/users/dashboard/collection"
+          className="bg-color-accent text-color-dark font-bold px-4 py-3 text-xl"
+        >
+          My Collection
+        </Link>
+
+        <Link
+          href="/users/dashboard/comment"
+          className="bg-color-accent text-color-dark font-bold px-4 py-3 text-xl"
+        >
+          My Comment
+        </Link>
+      </div>
     </div>
   );
 };
