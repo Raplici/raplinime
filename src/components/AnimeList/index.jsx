ba9c5d2a88f96c1a7e73fc2f05413bcd5c86ba7d
Rafli Ferdian <raflitsany35@gmail.com>
2023-12-19 18:43:25 +0700

    get release date for horizontal card & fix next image warning

--- src/components/AnimeList/index.jsx
+++ src/components/AnimeList/index.jsx
@@ -3,6 +3,15 @@ import Image from "next/image";
 import Link from "next/link";
 
 const AnimeList = ({ api, horizontal }) => {
+  function formatDate(dateString) {
+    // Parse the date string using the ISO 8601 format
+    const date = new Date(dateString);
+
+    // Get year
+    const year = date.getFullYear();
+    return `${year}`;
+  }
+
   const cardVertical = (
     <div className="grid gap-x-3 gap-y-5 grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7">
       {api.data?.map((anime, index) => {
@@ -12,13 +21,15 @@ const AnimeList = ({ api, horizontal }) => {
             key={index}
             className="flex flex-col gap-2 cursor-pointer text-color-primary transition-all hover:text-Red-60"
           >
-            <Image
-              src={anime.images.webp.large_image_url}
-              alt={anime.images.jpg.large_image_url}
-              width={256}
-              height={256}
-              className="object-cover object-center rounded-lg aspect-[3/4]"
-            />
+            <div className="w-auto h-auto">
+              <Image
+                src={anime.images.webp.large_image_url}
+                alt={anime.images.jpg.large_image_url}
+                width={256}
+                height={256}
+                className="object-cover object-center rounded-lg aspect-[3/4]"
+              />
+            </div>
 
             <div className="flex flex-col gap-1">
               {anime != null && (
@@ -45,7 +56,7 @@ const AnimeList = ({ api, horizontal }) => {
     </div>
   );
   const cardHorizontal = (
-    <div className="relative h-max w-full xl:w-80 shrink-0 flex flex-col gap-3">
+    <div className="h-max w-full xl:w-80 shrink-0 flex flex-col gap-3">
       {api.data?.map((anime, index) => {
         return (
           <Link
@@ -53,22 +64,26 @@ const AnimeList = ({ api, horizontal }) => {
             href={`/anime/${anime.mal_id}`}
             className="flex flex-row cursor-pointer bg-Black-10 rounded-lg text-Absolute-White items-center transition hover:bg-gradient-to-r from-transparent to-Red-60"
           >
-            <Image
-              src={anime.images.webp.large_image_url}
-              alt={anime.images.jpg.large_image_url}
-              width={64}
-              height={100}
-              className="object-cover object-center rounded-l-lg aspect-[3/4]"
-            />
+            <div className="w-auto h-auto">
+              <Image
+                src={anime.images.webp.large_image_url}
+                alt={anime.images.jpg.large_image_url}
+                width={64}
+                height={100}
+                className="object-cover object-center rounded-l-lg aspect-[3/4] "
+              />
+            </div>
 
             <section className="flex flex-col px-5 w-full">
-              {anime.rating && (
+              {anime.aired && (
                 <div className="flex gap-3 text-sm text-Grey-60">
-                  <p>{anime.rating}</p>
+                  <p>{formatDate(anime.aired.from)}</p>
                 </div>
               )}
 
-              <p className="font-medium md:text-lg line-clamp-1">{anime.title}</p>
+              <p className="font-medium md:text-lg line-clamp-1">
+                {anime.title}
+              </p>
             </section>
           </Link>
         );
