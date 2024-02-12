80bf4e629228ecc32dc81fd8823426e1d022a309
Rafli Ferdian <raflitsany35@gmail.com>
2024-02-12 15:32:35 +0700

    fix minor issues

--- src/app/(public)/season/page.jsx
+++ src/app/(public)/season/page.jsx
@@ -1,5 +1,5 @@
 import Navbar from "@/src/components/Navbar";
-import Seasonal from "@/src/app/season/Seasonal";
+import Seasonal from "@/src/app/(public)/season/Seasonal";
 
 const page = () => {
   return (
